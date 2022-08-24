3d6fc23e4163c9c5eab614126c1e94a0a338ea82
KonstantinOsipov <osipo38@yandex.ru>
2022-08-24 15:07:01 +0300

    save

---
+++ Exercise9_1/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+
+M = 4; N = 8. -> ""4, 6, 7, 8"" */
+
+void Numb(int m, int n)
+{
+ if(n > m) Numb(m, n - 1);
+    
+ if(m > n) Numb(m, n + 1);
+ Console.Write(n + " ");
+}
+
+Console.Write("Введите первое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Numb(m, n);
---
+++ Exercise9_3/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+m = 2, n = 3 -> A(m,n) = 29  */
+
+int Akkerman(int m, int n)
+{
+ if(m == 0) return n + 1;
+ if(n == 0) return Akkerman(m - 1, 1);
+ if(m != 0 && n != 0) return Akkerman(m - 1, Akkerman(m, n - 1));   
+ return 0;
+}
+
+Console.Write($"Введите положительное число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Введите положительное число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(m < 0 || n < 0) Console.WriteLine("Вы ввели отрицательное число");
+
+int akkerman = Akkerman(m, n);
+Console.WriteLine($"Функция аккермана равна: {akkerman}");
